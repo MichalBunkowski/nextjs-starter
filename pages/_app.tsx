dd3c31bcf6738a0f90db6ae457136b3b2f040dff
Michał Buńkowski <michalbunkowski@gmail.com>
2021-10-28 15:15:15 +0200

    Initial commit

---
+++ pages/_app.tsx
@@ -0,0 +1,11 @@
+import type { VFC } from 'react';
+
+import type { AppProps } from 'next/app';
+
+import '../styles/globals.css';
+
+const MyApp: VFC<AppProps> = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
+
+export default MyApp;
